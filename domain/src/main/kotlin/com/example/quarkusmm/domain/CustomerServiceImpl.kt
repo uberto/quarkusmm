019307e351f2c1d8605b54d7e635090d1c5748a6
Uberto Barbini <uberto.barbino@springernature.com>
2020-08-20 17:34:28 +0100

    removing code in the main module and added a readme.md

--- domain/src/main/kotlin/com/example/quarkusmm/domain/CustomerServiceImpl.kt
+++ domain/src/main/kotlin/com/example/quarkusmm/domain/CustomerServiceImpl.kt
@@ -6,6 +6,6 @@ import javax.enterprise.context.ApplicationScoped
 @ApplicationScoped
 class CustomerServiceImpl : CustomerService {
     override fun getMessage(): String {
-        return "howdy"
+        return "howdy dude leb"
     }
 }
